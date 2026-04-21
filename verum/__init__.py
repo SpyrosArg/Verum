f01eea2abcc0d786815ba264df43c18f55950c15
Spyros Argyrakos <spyros.argyrakos@gmail.com>
2026-04-21 13:32:03 +0300

    add verum package init

---
+++ verum/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    VerumResult,
+    seal,
+    bind,
+    verify,
+    export,
+    load,
+)
+
+__all__ = [
+    "VerumResult",
+    "seal",
+    "bind",
+    "verify",
+    "export",
+    "load",
+]
